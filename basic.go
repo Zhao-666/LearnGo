6d2a0c911fc2e5b20cb462b37a58ca5dcf2dae53
Zhao-666 <2268699370@qq.com>
2018-08-07 08:57:53 +0800

    define variables

---
+++ basic.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+var (
+	aa = 3
+	bb = 4
+)
+
+func variableEmptyValue() {
+	var a int
+	var s string
+	fmt.Println(a, s)
+}
+
+func variableInitValue() {
+	var a int = 123
+	var s string = "abc"
+	fmt.Println(a, s)
+}
+
+func variableTypeValue() {
+	var a, b, c, d = 123, 234, "abc", true
+	fmt.Println(a, b, c, d)
+}
+
+func variableSimpleValue() {
+	a, b, c, d := 123, 234, "abc", true
+	fmt.Println(a, b, c, d)
+}
+
+func main() {
+	fmt.Println("HelloWorld")
+	variableEmptyValue()
+	variableInitValue()
+	variableTypeValue()
+	variableSimpleValue()
+	fmt.Println(aa, bb)
+}
