45b746e675e968e43ef5b90ccda24019af8a40da
Zhao-666 <2268699370@qq.com>
2018-08-08 08:43:50 +0800

    FIX BUG

---
+++ loop.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strconv"
+	"fmt"
+)
+
+func convertToBin(n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(convertToBin(5))
+}
