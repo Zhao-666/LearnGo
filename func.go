40cddabbbf0db281c538e0171c9aa90978edf226
Zhao-666 <2268699370@qq.com>
2018-08-09 09:18:45 +0800

    1

---
+++ func.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"math"
+)
+
+func eval(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		return a / b
+	default:
+		panic("什么鬼" + op)
+	}
+}
+
+func apply(op func(int, int) int, a, b int) int {
+	p := reflect.ValueOf(op).Pointer()
+	opName := runtime.FuncForPC(p).Name()
+	fmt.Println(opName, a, b)
+	return op(a, b)
+}
+
+func pow(a,b int) int {
+	return int(math.Pow(float64(a),float64(b)))
+}
+
+func main() {
+	fmt.Println(eval(3, 4, "*"))
+	fmt.Println(apply(pow,3,4))
+}
